50217c9ee10178eb56d5fd08900d0729ca0eb72a
idmc <id.morales10n@gmail.com>
2019-05-13 23:58:55 -0500

    Primer commit

---
+++ server/config/config.js
@@ -0,0 +1,8 @@
+
+//configuracion del puerto cuando vamos a produccion y a desarrollo
+
+//========================================
+// PUERTO
+//========================================
+
+process.env.PORT= process.env.PORT || 3000;
